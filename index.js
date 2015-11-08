675be1101b8a0c8bc784285c525e23e928ba483c
Shuvalov Anton <anton_shuvalov@me.com>
2015-11-08 14:10:47 +0300

    add route logic

--- index.js
+++ index.js
@@ -3,17 +3,32 @@
 /**
  * Dependencies
  */
-const express = require('express');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const compilers    = require('./lib/compilers');
+const debug      = require('debug')('md-builder:app');
 
-const app = express();
+debug('App has registred compilers for %o', Object.keys(compilers));
 
-app
-  .post('/:format', () => {
-    // 1. parse markdown
-    // 2. Make some typography things
-    // 3. Compile to given format
-    // 4. Return result
-    // 5. …
-    // 6. PROFIT
+const app = module.exports = express()
+  .use(bodyParser.json())
+  .post('/:format', (req, res) => {
+
+    debug('params: %o', req.params);
+    debug('body: %o', req.body);
+
+    const format = req.params.format;
+    const parserFn = compilers[format.toLowerCase()];
+    if (!parserFn) return res.sendStatus(404);
+
+    debug('parser for %s has been found', format);
+    const data = parserFn({
+      content: req.body.content,
+      lang: req.body.lang || 'en'
+    });
+
+    debug('Markdown has been compiled to %s: %o', format, data);
+    return res.send({ data });
   })
+  .listen(8000, () => console.log('App listen 8000 port'))
 ;
