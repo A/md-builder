ead32203b55977453272b35a4614dee572c6560d
Shuvalov Anton <anton_shuvalov@me.com>
2015-11-09 03:25:02 +0300

    add test for pdf, update expected API

--- test/index.js
+++ test/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const request = require('supertest');
-const app = require('../');
+const app     = require('../');
+const assert  = require('assert');
 
 
 const agent = request.agent(app);
@@ -9,18 +10,34 @@ const agent = request.agent(app);
 
 describe('md-builder', () => {
 
-  it('should work', done => {
-    agent
-      .post('/html')
-      .send({
-        lang: 'en',
-        content: '# God in his heaven, all rights with the world!'
-      })
-      .expect(200, {
-        data: '<h1>God in his heaven, all rights with the world!</h1>'
-      })
-      .end(done)
-    ;
+  describe('HTML compiler', () => {
+    it('should make some HTML', done => {
+      agent
+        .post('/html')
+        .send({
+          lang: 'en',
+          content: '# God in his heaven, all rights with the world!'
+        })
+        .expect(200, '<h1>God in his heaven, all rights with the world!</h1>')
+        .end(done)
+      ;
+    });
+  });
+
+  describe('PDF compiler', () => {
+    it('should return PDF file', done => {
+      agent
+        .post('/pdf')
+        .send({
+          lang: 'en',
+          content: '# God in his heaven, all rights with the world!'
+        })
+        .expect(200)
+        .end((err, res) => {
+          assert(0 === res.text.indexOf('%PDF'), 'should be PDF')
+          done(err);
+        })
+    });
   });
 
 })
