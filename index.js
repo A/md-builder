d649defec3a33acd6e9c04c3df50def241d045fc
Shuvalov Anton <anton_shuvalov@me.com>
2015-11-12 00:23:33 +0300

    refactor compilers to new format to support MIME

--- index.js
+++ index.js
@@ -5,27 +5,42 @@
  */
 const express    = require('express');
 const bodyParser = require('body-parser');
-const compilers  = require('./lib/compilers');
+const logger     = require('morgan');
 const debug      = require('debug')('md-builder:app');
+const compilers  = require('./lib/compilers');
 
-debug('App has registred compilers for %o', Object.keys(compilers));
 
 const app = module.exports = express()
+
+  // Config
+  .set('views', './views')
+  .set('view engine', 'jade')
+
+  // Middlewares
+  .use(logger('dev'))
   .use(bodyParser.json())
+  .use(bodyParser.urlencoded())
+  .use(express.static('./public'))
+  .use((req, res, next) => {
+      debug('params: %o', req.params);
+      debug('body: %o', req.body);
+      next();
+  })
+
+  .get('/', (req, res) => res.render('index'))
+
   .post('/:format', (req, res) => {
-    debug('params: %o', req.params);
-    debug('body: %o', req.body);
-    const format = req.params.format;
-    const parserFn = compilers[format.toLowerCase()];
-    if (!parserFn) return res.sendStatus(404);
-    debug('parser for %s has been found', format);
-    const data = parserFn({
+    const compiler = compilers.get(req.params.format);
+    if (!compiler) return res.sendStatus(404);
+    compiler({
       content: req.body.content,
-      lang: req.body.lang || 'en'
-    }, (err, content) => {
-      debug('Markdown has been compiled to %s: %o', format, content);
-      res.send(content);
+      lang: req.body.lang
+    }, (err, data) => {
+      debug('Markdown has been compiled: %o', data.content);
+      res.set('Content-Type', data.mimetype || 'text/html');
+      res.send(data.content);
     });
   })
+
   .listen(8000, () => console.log('App listen 8000 port'))
 ;
