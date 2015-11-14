cf5e805695c8611c6d7304669831b79e14ba1aa7
Shuvalov Anton <anton_shuvalov@me.com>
2015-11-14 19:17:42 +0300

    add gist controller

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const logger     = require('morgan');
 const debug      = require('debug')('md-builder:app');
 const compilers  = require('./lib/compilers');
 const mime       = require('mime-types');
+const mongoose   = require('mongoose');
+const Gist       = require('./lib/models/gist');
+
+
+mongoose.connect('mongodb://localhost/test', (err) => {
+
+});
+
 
 const app = module.exports = express()
 
@@ -27,17 +35,35 @@ const app = module.exports = express()
       next();
   })
 
+  /**
+   * Render homepage
+   */
   .get('/', (req, res) => res.render('index'))
 
+  /**
+   * Compile posted data to the given format
+   */
   .post('/:format', (req, res) => {
     const format = req.params.format;
     const compiler = compilers.get(format);
     if (!compiler) return res.sendStatus(404);
     compiler(req.body, (err, data) => {
+      if (data.gist_id) return res.redirect('/gist/' + data.gist_id);
       debug('Markdown has been compiled: %o', data.content);
       res.set('Content-Type', mime.lookup(format) || 'text/html');
       res.send(data.content);
     });
+  })
+
+  /**
+   * Render gist by id
+   */
+  .get('/gist/:id', (req, res) => {
+    Gist
+      .findById(req.params.id, (err, gist) => {
+        res.render('gist', gist.toJSON());
+      })
+    ;
   })
 
   .listen(8000, () => console.log('App listen 8000 port'))
